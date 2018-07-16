52347d99cf9b090cbb054684f89d4e6fa1dbb500
Lachlan Sleight <lachlansleight@gmail.com>
2018-07-16 13:05:32 +1000

    Migrated Foliar.Compute over

---
+++ Assets/Foliar.Compute/Scripts/Buffer/BufferSetupWithDispatch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foliar.Compute {
+
+	public class BufferSetupWithDispatch : BufferSetup {
+
+		public ComputeDispatcher Dispatcher;
+		public int ThreadGroupsX;
+		public int ThreadGroupsY;
+		public int ThreadGroupsZ;
+
+		protected override void CreateData() {
+			Dispatcher.Dispatch(ThreadGroupsX, ThreadGroupsY, ThreadGroupsZ);
+		}
+	}
+}
---
+++ Assets/Foliar.Compute/Scripts/Parameters/ShaderValues.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foliar.Compute {
+
+	public class ShaderValues : ScriptableObject {
+
+		
+	}
+
+}
